d2ff2f92087be2b2bb19079022119fb95af5baa1
Francisco Fábio <franciscofabio18@hotmail.com>
2022-11-02 23:43:48 -0300

    02112022_7

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ def result():
 
 
 # app.run(debug=True, port=5000)
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
