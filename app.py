2a31cab61ab1b271164a2561012eec11f74a7f0a
Francisco Fábio <franciscofabio18@hotmail.com>
2022-11-02 23:32:29 -0300

    mudanca de port 8080

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def result():
 
 # app.run(debug=True, port=5000)
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
